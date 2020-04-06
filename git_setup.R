bcc0794d9249b1f295ad721c767ebe8e64965e36
Jeffrey Andrews <jeffrey_andrews@ecocn04.eva.mpg.de>
2020-04-06 15:36:32 +0200

    First commit

---
+++ git_setup.R
@@ -0,0 +1,9 @@
+##########GIT TERMINAL SETUP ###############
+
+
+echo "# market_integration" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/abjeffre/market_integration.git
+git push -u origin master
